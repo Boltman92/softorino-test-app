8241be81549464a7f318053e49b97739f0c8b158
razbeikov <razbeikov.oleksandr@gmail.com>
2023-11-09 16:52:19 +0200

    fix: delete unable file path

--- src/app/utils/read-file.ts
+++ src/app/utils/read-file.ts
@@ -3,7 +3,6 @@ import fsPromises from "fs/promises";
 import { UserData } from "../interfaces";
 
 export const dataFilePath = "./src/app/signup/api/userData.json";
-export const projectFilePath = "/src/app/projects/api/projectsData.json";
 
 export async function readUserDataFile() {
   const file = path.join(process.cwd(), dataFilePath);
